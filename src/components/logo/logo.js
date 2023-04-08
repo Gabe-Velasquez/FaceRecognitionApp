1576eb50e964f9e5596610b87a27b4eb10e0fa79
Gabe <gabriel.velasquez0116@gmail.com>
2023-04-08 12:42:20 -0600

    started logo and nav components

---
+++ src/components/logo/logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import robot from './robot.png';
+import './logo.css';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className='Tilt br shadow-2' options={{max: 65}} style={{ height: 200, width: 200 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '5px'}} src={robot} alt='logo'></img></div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
