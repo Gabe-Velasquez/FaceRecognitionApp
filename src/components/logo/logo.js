ce344160999d591713c7c13cd04fe7c90fa8ef12
Gabe <gabriel.velasquez0116@gmail.com>
2023-04-08 12:48:50 -0600

    fixing nav

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className='Tilt br shadow-2' options={{max: 65}} style={{ height: 200, width: 200 }}>
-                <div className='Tilt-inner pa3'><img style={{paddingTop: '5px'}} src={robot} alt='logo'></img></div>
+                <div className='Tilt-inner pa5'><img style={{paddingTop: '5px'}} src={robot} alt='logo'></img></div>
             </Tilt>
         </div>
     );
