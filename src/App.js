e2d61939f423ead8d82b4675ff3e99289a1a1d84
Gabe <gabriel.velasquez0116@gmail.com>
2023-04-13 16:36:51 -0600

    edited api integration and added facerecognition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Clarifai from 'clarifai';
+// import Clarifai from 'clarifai';
 import Navigation from './components/navigation/navigation';
+import FaceRecognition from './components/facerecognition/FaceRecognition';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imagelinkform.js';
 import Rank from './components/Rank/Rank';
@@ -8,27 +9,80 @@ import ParticlesBg from 'particles-bg';
 import 'tachyons';
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: '66e66da1b0c145848846e43a86159d84'
- });
+// const app = new Clarifai.App({
+//   apiKey: '66e66da1b0c145848846e43a86159d84'
+//  });
+//  const setupClarifai = (imageUrl) =>{
 
+//  }
+
+     ///////////////////////////////////////////////////////////////////////////////////////////////////
+     // In this section, we set the user authentication, user and app ID, model details, and the URL
+     // of the image we want as an input. Change these strings to run your own example.
+     //////////////////////////////////////////////////////////////////////////////////////////////////
+    const returnClarifaiRequestOptions = (imageUrl) => {
+     // Your PAT (Personal Access Token) can be found in the portal under Authentification
+     const PAT = '43edef596f624df58ec117a0a4ec58a1';
+     // Specify the correct user_id/app_id pairings
+     // Since you're making inferences outside your app's scope
+     const USER_ID = 'gvelas01';       
+     const APP_ID = 'Face-Recognition';
+     // Change these to whatever model and image URL you want to use
+     const MODEL_ID = 'face-detection';
+     const MODEL_VERSION_ID = '5e026c5fae004ed4a83263ebaabec49e';    
+     const IMAGE_URL = imageUrl;
+ 
+     ///////////////////////////////////////////////////////////////////////////////////
+     // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+     ///////////////////////////////////////////////////////////////////////////////////
+ 
+     const raw = JSON.stringify({
+         "user_app_id": {
+             "user_id": USER_ID,
+             "app_id": APP_ID
+         },
+         "inputs": [
+             {
+                 "data": {
+                     "image": {
+                         "url": IMAGE_URL
+                     }
+                 }
+             }
+         ]
+     });
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+  return requestOptions
+    }
+     
+ 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
-  onButtonSubmit =()=>{
-    console.log('click');
-    app.models.predict('face-detection', this.state.input)
+  onButtonSubmit = () => { 
+    this.setState({imageUrl: this.state.input})
+    // app.models.predict('face-detection', this.state.input)
+    fetch("https://api.clarifai.com/v2/models/" + 'face-dectection' + "/outputs", returnClarifaiRequestOptions(this.state.input))
+         .then(response => response.json())
     .then(response => {
-      console.log('hi', response)
+      console.log('meow', response)
       if (response){
         fetch('https://localhost:3000/image', {
           method: 'put',
@@ -39,7 +93,7 @@ class App extends Component {
         })
         .then(response => response.json())
         .then(count => {
-          this.setState(Object.assign(this.state.user, {entries:}))
+          this.setState(Object.assign(this.state.user, {entries: count}))
         })
       }
     })
@@ -54,7 +108,7 @@ class App extends Component {
       <Logo />
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-      {/* <FaceRecognition /> */}
+      <FaceRecognition imageUrl={this.state.imageUrl} />
     </div>
   );
   }
