c385ae10a71f079f02fabbb67dc9cd30d11aabac
Gabe <gabriel.velasquez0116@gmail.com>
2023-05-01 11:39:19 -0600

    updating register page for users over server

--- src/App.js
+++ src/App.js
@@ -59,7 +59,24 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false, 
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user:{
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -143,7 +160,7 @@ class App extends Component {
           : (
             route === 'signin' 
             ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )
           
         }
