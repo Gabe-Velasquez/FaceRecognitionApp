7ae51db2c8589748f4ec27fbead23020f7545149
Gabe <gabriel.velasquez0116@gmail.com>
2023-05-07 16:40:56 -0600

    setting initial state

--- src/App.js
+++ src/App.js
@@ -50,23 +50,24 @@ const returnClarifaiRequestOptions = (imageUrl) => {
   return requestOptions;
 }
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false, 
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    }
+  }
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false, 
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -134,7 +135,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route=== 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
   } else if (route === 'home') {
     this.setState({isSignedIn: true})
   }
